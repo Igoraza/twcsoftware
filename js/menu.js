a86a9e40bac2af4af8d90e094e9bfdc035aeaacf
Igoraza <86516457+Igoraza@users.noreply.github.com>
2023-03-04 12:10:13 +0530

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+	$(".mob_nav").click(function(){
+		$(this).toggleClass("open");
+		$(".header_menu").toggleClass("view");
+	});
+
+	$(".header_user").click(function() {
+		$(".user_drop").toggleClass("view");
+		$(".shadow_layer").toggleClass("view");
+	});
+	$(".shadow_layer").click(function() {
+		$("div").removeClass("view");
+		$("ul").removeClass("view");
+	});
+});
+
+
+const $body = $('body');
+
+const toggleOffcanvas = () => {
+	$body.toggleClass('has-offcanvas');
+}
+
+$('.hamburger-toggle').on('click', toggleOffcanvas);
